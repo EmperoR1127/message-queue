46a1cb2e376eedb3988e54b72efbb366dd157146
JoeWang1127 <51844517+EmperoR1127@users.noreply.github.com>
2025-07-19 21:39:22 -0400

    format

--- core/src/main/java/com/joewang/MessageQueue.java
+++ core/src/main/java/com/joewang/MessageQueue.java
@@ -4,39 +4,39 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class MessageQueue<E> {
-    private final BlockingQueue<E> messageQueue;
-    private final static int CAPACITY = 100;
+  private final BlockingQueue<E> messageQueue;
+  private static final int CAPACITY = 100;
 
-    private MessageQueue(BlockingQueue<E> queue) {
-        this.messageQueue = queue;
+  private MessageQueue(BlockingQueue<E> queue) {
+    this.messageQueue = queue;
+  }
+
+  public void put(E task) throws InterruptedException {
+    this.messageQueue.put(task);
+  }
+
+  public E get() throws InterruptedException {
+    return this.messageQueue.take();
+  }
+
+  public static class Builder<E> {
+    private BlockingQueue<E> messageQueue;
+
+    public Builder<E> newBuilder() {
+      this.messageQueue = new ArrayBlockingQueue<>(CAPACITY);
+      return this;
     }
 
-    public void put(E task) throws InterruptedException {
-        this.messageQueue.put(task);
+    public MessageQueue<E> build() {
+      return new MessageQueue<>(this.messageQueue);
     }
 
-    public E get() throws InterruptedException {
-        return this.messageQueue.take();
+    public Builder<E> setCapacity(int capacity) {
+      if (capacity > CAPACITY) {
+        this.messageQueue = new ArrayBlockingQueue<>(capacity);
+      }
+
+      return this;
     }
-
-    public static class Builder<E> {
-        private BlockingQueue<E> messageQueue;
-
-        public Builder<E> newBuilder() {
-            this.messageQueue = new ArrayBlockingQueue<>(CAPACITY);
-            return this;
-        }
-
-        public MessageQueue<E> build() {
-            return new MessageQueue<>(this.messageQueue);
-        }
-
-        public Builder<E> setCapacity(int capacity) {
-            if (capacity > CAPACITY) {
-                this.messageQueue = new ArrayBlockingQueue<>(capacity);
-            }
-
-            return this;
-        }
-    }
+  }
 }
--- core/src/test/java/com/joewang/MessageQueueTest.java
+++ core/src/test/java/com/joewang/MessageQueueTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class MessageQueueTest {
-    @Test
-    void testPutSuccessful() throws InterruptedException {
-        MessageQueue<String> queue = new MessageQueue.Builder<String>().newBuilder().build();
-        queue.put("a string");
-        assertEquals("a string", queue.get());
-    }
+  @Test
+  void testPutSuccessful() throws InterruptedException {
+    MessageQueue<String> queue = new MessageQueue.Builder<String>().newBuilder().build();
+    queue.put("a string");
+    assertEquals("a string", queue.get());
+  }
 }
