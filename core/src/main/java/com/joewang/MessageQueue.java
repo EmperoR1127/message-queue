c7e76ef7d94a1c5f934c5ca9bb5ca51735d84c21
JoeWang1127 <51844517+EmperoR1127@users.noreply.github.com>
2025-07-19 21:23:30 -0400

    feat: create a message queue

---
+++ core/src/main/java/com/joewang/MessageQueue.java
@@ -0,0 +1,42 @@
+package com.joewang;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class MessageQueue<E> {
+    private final BlockingQueue<E> messageQueue;
+    private final static int CAPACITY = 100;
+
+    private MessageQueue(BlockingQueue<E> queue) {
+        this.messageQueue = queue;
+    }
+
+    public void put(E task) throws InterruptedException {
+        this.messageQueue.put(task);
+    }
+
+    public E get() throws InterruptedException {
+        return this.messageQueue.take();
+    }
+
+    public static class Builder<E> {
+        private BlockingQueue<E> messageQueue;
+
+        public Builder<E> newBuilder() {
+            this.messageQueue = new ArrayBlockingQueue<>(CAPACITY);
+            return this;
+        }
+
+        public MessageQueue<E> build() {
+            return new MessageQueue<>(this.messageQueue);
+        }
+
+        public Builder<E> setCapacity(int capacity) {
+            if (capacity > CAPACITY) {
+                this.messageQueue = new ArrayBlockingQueue<>(capacity);
+            }
+
+            return this;
+        }
+    }
+}
